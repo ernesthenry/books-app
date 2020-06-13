9c791032cfd7041f51c67e7aca37340aaf13743c
ernesthenry <henry38ernest@gmail.com>
2020-06-13 21:01:45 +0300

    Updates

---
+++ precache-manifest.f6ff875caef52dabc96dbefa6e2cbb51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6977af7386ab497d06a1117385fdfff4",
+    "url": "/books-app/index.html"
+  },
+  {
+    "revision": "28ab86ba1d2bb04b493b",
+    "url": "/books-app/static/css/main.b45f327f.chunk.css"
+  },
+  {
+    "revision": "28ea433e1d915289d875",
+    "url": "/books-app/static/js/2.926fd58a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/books-app/static/js/2.926fd58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ab86ba1d2bb04b493b",
+    "url": "/books-app/static/js/main.021b2622.chunk.js"
+  },
+  {
+    "revision": "5e2c6fd56486f470ce3e",
+    "url": "/books-app/static/js/runtime-main.47686b51.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/books-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/books-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/books-app/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
